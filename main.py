1150c21b959c63a9691be00bc85faab1e65e609e
Africk-Hunter <hunterafrick@gmail.com>
2025-05-09 23:24:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+def run_application():
+    print('Hello')
+    return
+    # Initalize Characters
+    # Option to Create Characters
+    
+    ## Time Engine
+        ## Advance Time (Duh)
+        ## Store current time in a variable in this function
+        ## That variable is passed Down to the next function which will do something based on that time
+        
+    ## Schedule Handler:
+        ## Characters are allotted X actions per hour
+            ## Every tick (represetned as 60 tick in an hour) has a chance for an action to be done:
+                ## If a character triggers an action, a pool is created for the character based on various conditions
+                ## One option will be taken from the pool and displayed as an action.
+
+
+if __name__ == '__main__':
+    run_application()
